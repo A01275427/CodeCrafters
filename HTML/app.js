0c949a14966149fad3ca8ec6353d058278293cda
Levi <Daz>
2024-03-08 16:13:42 -0600

    primer commit

---
+++ HTML/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use((request, response, next) => {
+    console.log('Test');
+    next();
+})
+
+
+const rutasLogin = require('./routes/login.routes');
+app.use('/user', rutasLogin);
+
+const rutasHome = require('./routes/home.routes');
+app.use('/', rutasHome);
+
+const rutasDeuda = require('./routes/deuda.routes');
+app.use('/user', rutasDeuda);
+
+
+app.listen(3000);
