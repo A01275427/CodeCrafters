0973a1bb96f47a2ef52a64119f1971df86ef9695
Levi <Daz>
2024-03-27 15:19:31 -0600

    login y registrar

--- HTML/app.js
+++ HTML/app.js
@@ -4,6 +4,14 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views','views');
 
+const session = require('express-session');
+
+app.use(session({
+    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+  }));
+
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,14 +19,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 
+const csrf = require('csurf');
+const csrfProtection = csrf();
+app.use(csrfProtection); 
+
 app.use((request, response, next) => {
     console.log('Test');
     next();
 })
 
 
-const rutasLogin = require('./routes/login.routes');
-app.use('/user', rutasLogin);
+const rutasUsuario = require('./routes/usuario.routes');
+app.use('/user', rutasUsuario);
 
 const rutasHome = require('./routes/home.routes');
 app.use('/', rutasHome);
--- HTML/routes/pago_alumno.routes.js
+++ HTML/routes/pago_alumno.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 
-router.get('/pago', (request, response, next) => {
+router.get('/pago', isAuth, (request, response, next) => {
     response.render('pago_alumno');
 })
 
