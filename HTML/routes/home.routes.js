d61a28ad2b7fb1fe62105727aa79604bbd085f67
Ernesto <ernestoar10@hotmail.com>
2024-04-02 10:55:21 -0600

    Lógica de pago alumno sin recibir datos o enviarlos

--- HTML/routes/home.routes.js
+++ HTML/routes/home.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 
-router.get('/', (request, response, next) => {
+router.get('/', isAuth, (request, response, next) => {
     response.render('home');
 })
 
