32b3147f23c592206838df23b6938be05e9f6b60
Arturo <a01275427@tec.mx>
2024-04-05 02:57:14 -0600

    Ya se descarga PDF

--- HTML/routes/pago.routes.js
+++ HTML/routes/pago.routes.js
@@ -1,8 +1,12 @@
+// En pago.routes.js
 const express = require('express');
 const router = express.Router();
 const pagoController = require('../controllers/pago.controller');
 
-router.get('/historial_pagos', pagoController.getPaymentHistory);
-router.get('/user/descargar-historial', pagoController.downloadPaymentHistory);
+// Ajusta la ruta para incluir el parámetro :userID
+router.get('/historial_pagos/:userID', pagoController.getPaymentHistory);
+
+// Ajusta la ruta para incluir el parámetro :userID
+router.get('/descargar-historial/:userID', pagoController.downloadPaymentHistory);
 
 module.exports = router;
