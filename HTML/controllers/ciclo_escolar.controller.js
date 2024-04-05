f6cbd6935df1fd1ca760f7bb9073cca8683bca4d
Ernesto <ernestoar10@hotmail.com>
2024-04-04 22:46:14 -0600

    actualizar ruta admin

--- HTML/controllers/ciclo_escolar.controller.js
+++ HTML/controllers/ciclo_escolar.controller.js
@@ -1,5 +1,9 @@
 const Ciclo_escolar = require('../models/ciclo_escolar.model');
 
+exports.get_nuevo_ciclo = (request, response, next) => {
+    response.render('admin_dashboard')
+}
+
 exports.post_nuevo_ciclo = (request, response, next) => {
     const nuevo_ciclo = new Ciclo_escolar(request.body.Ciclo, request.body.Fecha_inicio, request.body.Fecha_fin, estaEnRango, request.costoCredito );
     nuevo_ciclo.save()
--- HTML/util/database.js
+++ HTML/util/database.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'arturosanchezrodriguez_viadisenio',
+    database: 'viadisenio',
     password: '',
-    port: 3306,
+    port: 3307,
 });
 
 module.exports = pool.promise();
