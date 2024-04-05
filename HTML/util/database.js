da8f2b023a70813b56fdf421a77135eda8d8e1e3
FaKeArtur27 <arsanmx@gmail.com>
2024-04-04 19:30:13 -0600

    Puerto

--- HTML/util/database.js
+++ HTML/util/database.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'viadisenio',
+    database: 'arturosanchezrodriguez_viadisenio',
     password: '',
-    port: 3307,
+    port: 3306,
 });
 
 module.exports = pool.promise();
